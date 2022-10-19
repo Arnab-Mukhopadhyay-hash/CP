807df9cbf1a75978e69e081f206242064660e436
Codie <arnabmukhopadhyay76@gmail.com>
2022-10-19 19:39:30 +0530

    First and Last Digit

---
+++ Code Chef/C++/FLOW004.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int sum = 0;
+        sum += n % 10;
+        while (n >= 10)
+        {
+            n /= 10;
+        }
+        sum += n;
+        cout << sum << endl;
+    }
+    return 0;
+}
