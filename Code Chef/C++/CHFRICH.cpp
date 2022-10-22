82eeafe07f3f6183d9cc90a5c04fcc9c99914540
Arnab-Mukhopadhyay-hash <84343247+Arnab-Mukhopadhyay-hash@users.noreply.github.com>
2022-10-22 10:48:05 +0530

    Richie Rich

---
+++ Code Chef/C++/CHFRICH.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int a, b, x;
+		cin>>a>>b>>x;
+		int ans = (b-a)/x;
+		cout<<ans<<endl;
+	}
+	return 0;
+}
