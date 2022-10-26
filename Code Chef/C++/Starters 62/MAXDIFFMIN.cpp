d76abe388c11e1df0d268b02fe5260d9c03822cd
Arnab Mukhopadhyay <arnabmukhopadhyay76@gmail.com>
2022-10-26 20:38:06 +0530

    Max Minus Min

---
+++ Code Chef/C++/Starters 62/MAXDIFFMIN.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int a, b, c;
+		cin>>a>>b>>c;
+		int maxx = c;
+		int minn = a;
+		cout<<maxx-minn<<endl;
+	}
+	return 0;
+}
