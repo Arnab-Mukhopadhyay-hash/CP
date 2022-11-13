380017c229e9c17296d11928ccb95ea758a3f587
Arnab Mukhopadhyay <arnabmukhopadhyay76@gmail.com>
2022-11-13 19:54:08 +0530

    Water Mixing

---
+++ Code Chef/C++/WTRMIXING.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a, b, x, y;
+	    cin>>a>>b>>x>>y;
+	    if(b > a){
+	        if(b-a <= x) cout<<"YES"<<endl;
+	        else cout<<"NO"<<endl;
+	    }
+	    else if(b < a){
+	        if(a-b<=y)cout<<"YES"<<endl;
+	        else cout<<"NO"<<endl;
+	    }
+	    else cout<<"YES"<<endl;
+	}
+	return 0;
+}
